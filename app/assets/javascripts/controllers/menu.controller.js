bfa6da786a3ecbad8ecb84d09ecfdd4301debf15
Tu Hoang <hatu87@gmail.com>
2016-03-14 19:03:38 +0700

    edit config

--- app/assets/javascripts/controllers/menu.controller.js
+++ app/assets/javascripts/controllers/menu.controller.js
@@ -1,14 +1,12 @@
 (function() {
   'use strict';
-  angular.module('ken-restaurant').controller('MenuController', ['$scope', 'FoodService', function($scope, FoodService) {
+  angular.module('ken-restaurant', ['truncate']).controller('MenuController', ['$scope', 'FoodService', function($scope, FoodService) {
     $scope.name = 'ken';
     $scope.foods = [];
     $scope.selectedSectionId = 0;
     $scope.selectedSortMode = 0;
-    
     $scope.getFoodsBySection = function(sectionId) {
       $scope.selectedSectionId = sectionId;
-      
       console.log('getting foods by section ' + sectionId);
       FoodService.getFoodsBySection(sectionId).success(function(data) {
         console.log(data);
@@ -23,16 +21,13 @@
       // debugger;
       $scope.selectedFood = $scope.foods[selectedIndex];
     };
-
     $scope.rating = 4;
     $scope.maxRating = 5;
-    $scope.range = function(number){
+    $scope.range = function(number) {
       return new Array(number);
     }
-
     $scope.sortFoods = function(sortMode) {
       $scope.selectedSortMode = sortMode;
-
       FoodService.searchFoods($scope.selectedSectionId, sortMode).success(function(data) {
         console.log(data);
         $scope.foods = data;
